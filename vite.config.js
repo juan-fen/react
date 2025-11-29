362ca643856e6e4f6adb011e8884c6d7878d45d6
Juan <juan@learnfrontendnow.com>
2025-11-29 11:58:15 -0500

    Rollback vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 🚨 You must replace 'your-repository-name' with the EXACT name
-// of your GitHub repository. It must start and end with a slash!
-const repoName = "/react/";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use the repository name as the base path
-  base: repoName,
 });
