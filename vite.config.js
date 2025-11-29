a39d7f2f2ca3dc960f0afaf233615efc3fbcf314
Juan <juan@learnfrontendnow.com>
2025-11-29 12:09:11 -0500

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Replace 'your-repository-name' with the EXACT name of your GitHub repo
+const repoName = "/react/";
+
 export default defineConfig({
   plugins: [react()],
+  // 💡 THIS LINE FIXES THE 404 ASSET ISSUE
+  base: repoName,
 });
