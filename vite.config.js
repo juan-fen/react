c588cd028b53b3b6ca777a8eb18a799799857be2
Juan <juan@learnfrontendnow.com>
2025-11-29 11:48:36 -0500

    Add vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// 🚨 You must replace 'your-repository-name' with the EXACT name
+// of your GitHub repository. It must start and end with a slash!
+const repoName = "/react/";
+
 export default defineConfig({
   plugins: [react()],
-})
+  // Use the repository name as the base path
+  base: repoName,
+});
